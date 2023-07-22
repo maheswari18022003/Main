bb6a6a054b7951c2824432233782e3de2b963cb4
Maheswari <maheswari18022003@gmail.com>
2023-07-22 11:34:30 +0530

    file no 23

---
+++ src/psr.java
@@ -0,0 +1,21 @@
+public class psr {
+    int age;
+    String name;
+    String department;
+
+    public void getInput( String name,String department,int age){
+        this.age = age;
+        this.name = name;
+        this.department = department;
+
+                public void printer() {
+            System.out.println("name is + "name");
+            System.out.println("department is + "department");
+            System.out.println("age is" + "age");
+
+
+        }
+
+    }
+
+}
